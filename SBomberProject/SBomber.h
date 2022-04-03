711d877df453eb722499578c87cbef41493e1a86
Frost <staikartur@mail.ru>
2022-04-03 11:08:32 +0500

    addTask1-1

--- SBomberProject/SBomber.h
+++ SBomberProject/SBomber.h
@@ -1,23 +1,23 @@
 #pragma once
 
 #include <vector>
-
+#include <iostream>
 #include "LevelGUI.h"
 #include "Plane.h"
 #include "Bomb.h"
 #include "Ground.h"
 #include "Tank.h"
-
-
+#include "MyTools.h"
 
 class SBomber
 {
 public:
 
-    SBomber();
+    SBomber(MyTools::ILogger& logger);
     ~SBomber();
     
     inline bool GetExitFlag() const { return exitFlag; }
+    friend class BombIterator;
 
     void ProcessKBHit();
     void TimeStart();
@@ -27,6 +27,119 @@ public:
     void MoveObjects();
     void CheckObjects();
 
+    DynamicObject* begin() {
+        return *vecDynamicObj.begin();
+   }
+
+    DynamicObject* end() {
+        return *vecDynamicObj.end();
+    }
+
+
+    class BombIterator {
+        const std::vector<DynamicObject*>& VecDynObj;
+        Bomb* Current;
+        int curIndex;
+        int* ptr;
+
+    public:
+        BombIterator(const std::vector<DynamicObject*>& vec) : VecDynObj(vec), curIndex(-1), ptr(nullptr)
+        {
+            ++(*this);
+        }
+
+        void reset() { curIndex = -1; ptr = nullptr; }
+
+        BombIterator& operator++ () {
+            curIndex++;
+            if (curIndex == -1)
+                curIndex = 0;
+            for (; curIndex < VecDynObj.size(); curIndex++)
+            {
+                if (typeid(VecDynObj[curIndex]) == typeid(Current))
+                {
+                    //ptr = &vecDynObj[curIndex];
+                    break;
+                }
+            }
+            if (curIndex == VecDynObj.size())
+            {
+                curIndex = -1;
+                ptr = nullptr;
+            }
+            return *this;
+        }
+
+        BombIterator& operator-- () // префексный декремент
+        {
+            if (curIndex == -1)
+                curIndex = VecDynObj.size() - 1;
+            for (; curIndex >= 0; curIndex--)
+            {
+                if (typeid(VecDynObj[curIndex]) == typeid(Bomb*))
+                {
+                    // ptr = &vecDynObj[curIndex];
+                    break;
+                }
+            }
+            if (curIndex == -1)
+            {
+                ptr = nullptr;
+            }
+            return *this;
+        }
+
+        Bomb* operator*() // операция разыменования итератора
+        {
+
+            //return VecDynObj.at(curIndex);
+            return (Bomb*)(VecDynObj[curIndex]);
+        }
+
+
+        bool operator==(BombIterator it) // проверка на лог. равенство итераторов
+        {
+            if (curIndex == it.curIndex &&
+                ptr == it.ptr &&
+                VecDynObj == it.VecDynObj)
+            {
+                return true;
+            }
+            return false;
+        }
+        bool operator!=(BombIterator it) // проверка на лог. неравенство
+        {
+            return !(*this == it);
+        }
+
+
+        void myReset() {
+            curIndex++;
+            if (curIndex == -1)
+                curIndex = 0;
+            for (; curIndex < VecDynObj.size(); curIndex++)
+            {
+                if (typeid(VecDynObj[curIndex]) == typeid(Current))
+                {
+                    //ptr = &vecDynObj[curIndex];
+                    continue;
+                }
+            }
+
+        }
+    
+
+        // получаем итератор настроенный на начало массива
+        BombIterator begin() { BombIterator it(VecDynObj); return it; }
+        // итератор в конечном состоянии
+        BombIterator end() { 
+            BombIterator it(VecDynObj);
+             
+            it.myReset();
+
+            return it; }
+    };
+
 
 
 private:
@@ -35,27 +148,16 @@ private:
     void CheckBombsAndGround();
     void __fastcall CheckDestoyableObjects(Bomb* pBomb);
 
-    //void __fastcall DeleteDynamicObj(DynamicObject * pBomb);
-    //void __fastcall DeleteStaticObj(GameObject* pObj);
+    void __fastcall DeleteDynamicObj(DynamicObject * pBomb);
+    void __fastcall DeleteStaticObj(GameObject* pObj);
 
     Ground * FindGround() const;
-	Plane* FindPlane() const {
-		for (size_t i = 0; i < vecDynamicObj.size(); i++)
-		{
-			Plane* p = dynamic_cast<Plane*>(vecDynamicObj[i]);
-			if (p != nullptr)
-			{
-				return p;
-			}
-		}
-
-		return nullptr;
-	};
+    Plane * FindPlane() const;
     LevelGUI * FindLevelGUI() const;
     std::vector<DestroyableGroundObject*> FindDestoyableGroundObjects() const;
     std::vector<Bomb*> FindAllBombs() const;
 
-    //void DropBomb();
+    void DropBomb();
 
     std::vector<DynamicObject*> vecDynamicObj;
     std::vector<GameObject*> vecStaticObj;
@@ -65,91 +167,6 @@ private:
     uint64_t startTime, finishTime, passedTime;
     uint16_t bombsNumber, deltaTime, fps;
     int16_t score;
-
-public:
-
-	class Command
-	{
-	public:
-		Command() { }
-		virtual ~Command() {}
-		virtual void Execute() = 0;
-
-	};
-
-	template <class Object>
-	class DeleteCommand : public Command
-	{
-	public:
-		DeleteCommand(std::vector<Object*>& vecObj) : m_vecObj(vecObj), m_pObj(nullptr) {}
-		void setObj(Object* pObj)
-		{
-			m_pObj = pObj;
-		}
-		void Execute()
-		{
-			if (!m_pObj)
-			{
-				return;
-			}
-
-			auto it = m_vecObj.begin();
-			for (; it != m_vecObj.end(); it++)
-			{
-				if (*it == m_pObj)
-				{
-					m_vecObj.erase(it);
-					break;
-				}
-			}
-		}
-	private:
-		std::vector<Object*>& m_vecObj;
-		Object* m_pObj;
-	};
-
-	class DropCommand : public Command
-	{
-	public:
-		DropCommand(std::vector<DynamicObject*>& vecDynamicObj) :  m_vecDynamicObj(vecDynamicObj), m_pPlane(nullptr), m_bombsNumber(nullptr), m_score(nullptr) {}
-		void setParams(Plane* pPlane, uint16_t* bombsNumber, int16_t* score)
-		{
-			m_pPlane = pPlane;
-			m_bombsNumber = bombsNumber;
-			m_score = score;
-		}
-		void Execute()
-		{
-			
-			if (*m_bombsNumber > 0)
-			{
-				
-				//Logger.WriteToLog(string(__FUNCTION__) + " was invoked");
-
-				//const Plane* pPlane = this->FindPlane();
-				double x = m_pPlane->GetX() + 4;
-				double y = m_pPlane->GetY() + 2;
-
-				Bomb* pBomb = new Bomb;
-				pBomb->SetDirection(0.3, 1);
-				pBomb->SetSpeed(2);
-				pBomb->SetPos(x, y);
-				pBomb->SetWidth(SMALL_CRATER_SIZE);
-
-				m_vecDynamicObj.push_back(pBomb);
-				m_bombsNumber--;
-				m_score -= Bomb::BombCost;
-			}
-		}
-
-	private:
-		std::vector<DynamicObject*>& m_vecDynamicObj;
-		Plane* m_pPlane;
-		uint16_t* m_bombsNumber;
-		int16_t* m_score;
-	};
-
-
-
-
+    MyTools::ILogger& logger;
 };
+
